2ee1e9b4ab92b383a54c78f7a9cedefc07ceef13
Zac Crumpton <z.crumpton@gmail.com>
2025-08-06 01:06:02 -0500

    jk now i have fixed the alignment issue on contact us form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -38,15 +38,16 @@ export default function Contact() {
       <div className="max-w-6xl mx-auto px-4 pb-24">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
           {/* Contact Form */}
-          <div className="space-y-6 text-sm">
-          <h2 className="text-xl font-semibold font-typewriter text-white mb-4 -rotate-2 tracking-widest">
-              Contact Us
-            </h2>
+          
           <form
             action="https://formspree.io/f/xanbpvjl"
             method="POST"
             className="space-y-6 text-sm"
           >
+            <h2 className="text-xl font-semibold font-typewriter text-white mb-4 -rotate-2 tracking-widest">
+              Contact Us
+            </h2>
+            
             <input
               type="text"
               name="name"
@@ -74,7 +75,7 @@ export default function Contact() {
               Send Message
             </button>
           </form>
-          </div>
+
 
           {/* Mailing List Signup */}
           <div className="space-y-6 text-sm">
